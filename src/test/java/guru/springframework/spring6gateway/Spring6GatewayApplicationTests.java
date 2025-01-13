9306f1af6efacce119a5da2fc197358d0e0f00b6
Dominique Boeckli <dboeckli@gmail.com>
2025-01-13 09:39:06 +0100

    refactor project (#13)

--- src/test/java/guru/springframework/spring6gateway/Spring6GatewayApplicationTests.java
+++ src/test/java/guru/springframework/spring6gateway/Spring6GatewayApplicationTests.java
@@ -1,13 +1,16 @@
 package guru.springframework.spring6gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+@Slf4j
 class Spring6GatewayApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("Testing Spring 6 Template Application...");
     }
 
 }
